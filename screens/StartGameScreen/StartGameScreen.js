e6ea938950d6e248d12df48b0f4286728fc4982e
Kristian Vasilev <k.vasilev@xssoftware.com>
2022-03-12 13:25:13 +0200

    validate number input

--- screens/StartGameScreen/StartGameScreen.js
+++ screens/StartGameScreen/StartGameScreen.js
@@ -8,14 +8,22 @@ import {
 } from 'react-native';
 import Card from '../../components/Card';
 import Colors from '../../constants/colors';
+import Input from '../../components/Input';
 
 const StartGameScreen = () => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a new game!</Text>
-            <Card style={styles.inputContainer} >
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput placeholder='What is your number' />
+                <Input
+                    blurOnSubmit
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    keyboardType='number-pad'
+                    maxLength={2}
+                    style={styles.input}
+                />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
                         <Button 
@@ -57,6 +65,10 @@ const styles = new StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
+    },
+    input: {
+        width: 50,
+        textAlign: 'center',
     },
     button: {
         width: 100,
