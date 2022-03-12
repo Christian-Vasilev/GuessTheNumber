87ada4ddaae044e9a6587d0461e9f44e46a5359e
Kristian Vasilev <k.vasilev@xssoftware.com>
2022-03-12 11:42:41 +0200

    Add the game screen component

---
+++ screens/StartGameScreen/StartGameScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+const StartGameScreen = () => {
+    return (
+        <View style={styles.screen}>
+            <Text>The game screen</Text>
+        </View>
+    );
+}
+
+const styles = new StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+});
+
+export default StartGameScreen;
