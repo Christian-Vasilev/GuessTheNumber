ddb5c5ebe7244339dc54c3db6d2c4cb35d742c28
Kristian Vasilev <k.vasilev@xssoftware.com>
2022-03-12 12:14:12 +0200

    base styles from game screen

--- screens/StartGameScreen/StartGameScreen.js
+++ screens/StartGameScreen/StartGameScreen.js
@@ -2,13 +2,23 @@ import React from 'react';
 import {
     View,
     Text,
+    TextInput,
+    Button,
     StyleSheet
 } from 'react-native';
 
 const StartGameScreen = () => {
     return (
         <View style={styles.screen}>
-            <Text>The game screen</Text>
+            <Text style={styles.title}>Start a new game</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput placeholder='What is your number' />
+                <View style={styles.buttonContainer}>
+                    <Button title='Reset' color='red' onPress={() => {}}/>
+                    <Button title='Confirm' color='green' onPress={() => {}} />
+                </View>
+            </View>
         </View>
     );
 }
@@ -19,6 +29,21 @@ const styles = new StyleSheet.create({
         padding: 10,
         alignItems: 'center',
     },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    }
 });
 
 export default StartGameScreen;
