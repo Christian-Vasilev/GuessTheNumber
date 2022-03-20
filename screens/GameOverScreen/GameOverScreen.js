7f9c146be28d24cedb5597428035c72abdb05153
Kristian Vasilev <k.vasilev@xssoftware.com>
2022-03-20 12:27:41 +0200

    add endgame screen image

--- screens/GameOverScreen/GameOverScreen.js
+++ screens/GameOverScreen/GameOverScreen.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {
     View,
-    Text,
+    Image,
     Button,
     StyleSheet
 } from 'react-native';
+import BodyText from '../../components/BodyText';
+import TitleText from '../../components/TitleText';
 
 const GameOverScreen =  ({ totalRounds, chosenNumber, restartGame }) => {
     return (
         <View style={styles.screen}>
-            <Text>The game is over!</Text>
-            <Text>Number of rounds: {totalRounds}</Text>
-            <Text>Numbre was: {chosenNumber}</Text>
+
+            <TitleText>The game is over!</TitleText>
+            <Image 
+                style={styles.image} 
+                source={require('../../assets/success.png')}
+                resizeMode="contain"
+            />
+            <BodyText>Number of rounds: {totalRounds}</BodyText>
+            <BodyText>Numbre was: {chosenNumber}</BodyText>
             <Button title='Restart game' onPress={restartGame}/>
         </View>
     );
@@ -22,6 +30,10 @@ const styles = new StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        width: '80%',
+        height: 300
     }
 });
 
